96416a2a33d64fd86362475268673e77f7595820
Peter Ryabchenko <ryabchenko.peter@gmail.com>
2018-12-09 21:08:13 +0100

    added spectrogram computation

--- IAMI.py
+++ IAMI.py
@@ -1,25 +1,42 @@
 import librosa
+import numpy as np
 
-def loadAudio():
-    filePath = 'music_data/shortName.flac'
-    y, sr = librosa.load(filePath, sr=96000)
+
+def load_audio(file_path):
+    y, sr = librosa.load(file_path, sr=96000)
     print("sample values: ", y)
     print("sample #: ", len(y))
     print("sampling rate: ", sr)
     print()
+    return y
 
-    framesMatrix = librosa.util.frame(y, frame_length=2048, hop_length=1024)
-    print('frame size:', len(framesMatrix))
-    print('frame #', len(framesMatrix[0]))
+
+def compute_frames(y):
+    frames_matrix = librosa.util.frame(y, frame_length=2048, hop_length=1024)
+    print('frame size:', len(frames_matrix))
+    print('frame #', len(frames_matrix[0]))
     print()
     """for i, f in enumerate(framesMatrix):
         print(i, f[0])"""
+    return frames_matrix
 
+
+def compute_spectrogram(y):
+    spectrogram = librosa.core.stft(y=y, n_fft=2048, hop_length=1024)
+    spectrogramReal = np.abs(spectrogram)
+    """"
+    print(spectrogram[0])
+    print()
+    print(spectrogramReal[0])
+    """
+    return spectrogramReal
 
 
 def main():
-    print("IAMI executed as main")
-    loadAudio()
+    file_path = 'music_data/shortName.flac'
+    y = load_audio(file_path)
+    spectrogram = compute_spectrogram(y)
+
 
 if __name__ == "__main__":
     main()
