d8eddaf5d36b973e397162b430a6d4c45e046d80
Peter Ryabchenko <ryabchenko.peter@gmail.com>
2018-12-12 03:38:41 +0100

    refactored modules

--- IAMI.py
+++ IAMI.py
@@ -1,64 +1,12 @@
 import librosa
 import numpy as np
-
-
-def load_audio(file_path):
-    y, sr = librosa.load(file_path, sr=96000)
-    print("sample values: ", y)
-    print("sample #: ", len(y))
-    print("sampling rate: ", sr)
-    print()
-    return y
-
-
-# split input vector into overlapping frames (unused)
-def compute_frames(y):
-    frames_matrix = librosa.util.frame(y, frame_length=2048, hop_length=1024)
-    print('frame size:', len(frames_matrix))
-    print('frame #', len(frames_matrix[0]))
-    print()
-    """for i, f in enumerate(framesMatrix):
-        print(i, f[0])"""
-    return frames_matrix
-
-
-# apply short time fourier transform
-# take absolute values and square them
-# returns power spectrogram
-def compute_spectrogram(y):
-    spectrogram = librosa.core.stft(y=y, n_fft=2048, hop_length=1024)
-    power_spectrogram = (np.abs(spectrogram))**2
-    """"
-    print(spectrogram[0])
-    print()
-    """
-    #print(len(power_spectrogram), len(power_spectrogram[0]))
-    #print('spectrogram first element:\n\n', spectrogram_real[0], '\n\n\n')
-    return power_spectrogram
-
-
-# apply mel scale to spectrogram (with 40 bins, also try 80)
-def mel_transform(spectrogram):
-    mel_spectrogram = librosa.feature.melspectrogram(S=spectrogram, n_mels=40)
-    #print(len(mel_spectrogram), len(mel_spectrogram[0]))
-    #print('mel spectrogram first element:\n\n', mel_spectrogram[0], '\n\n\n')
-    return mel_spectrogram
-
-
-# apply a log10 scale to spectrogram (resulting magnitudes are in decibels)
-def log_scale(spectrogram):
-    log_mel_spectrogram = librosa.power_to_db(S=spectrogram)
-    #print(len(log_mel_spectrogram), len(log_mel_spectrogram[0]))
-    #print('log mel spectrogram first element:\n\n', log_mel_spectrogram[0], '\n\n\n')
-    return log_mel_spectrogram
+import feature as f
 
 
 def main():
     file_path = 'music_data/shortName.flac'
-    y = load_audio(file_path)
-    spectrogram = compute_spectrogram(y)
-    mel_spectrogram = mel_transform(spectrogram)
-    log_mel_spectrogram = log_scale(mel_spectrogram)
+    y = f.load_audio(file_path)
+    log_mel_spectrogram = f.compute_spectrogram(y, 2048, 1024, 40, True, True, True)
 
 
 if __name__ == "__main__":
---
+++ feature.py
@@ -0,0 +1,75 @@
+import librosa
+import numpy as np
+
+
+######## PART 1 ########
+
+
+def load_audio(file_path, inf=False):
+    y, sr = librosa.load(file_path, sr=96000)
+    if inf:
+        print("\n\nSample values: ", y)
+        print("sample #: ", len(y))
+        print("sampling rate: ", sr, '\n\n\n')
+    return y
+
+
+# split input vector into overlapping frames (unused)
+def compute_frames(y):
+    frames_matrix = librosa.util.frame(y, frame_length=2048, hop_length=1024)
+    print('frame size:', len(frames_matrix))
+    print('frame #', len(frames_matrix[0]))
+    print()
+    """for i, f in enumerate(framesMatrix):
+        print(i, f[0])"""
+    return frames_matrix
+
+
+# apply short time fourier transform
+# take absolute values and square them
+# returns power spectrogram
+def compute_power_spectrogram(y, frame_length=2048, hop_length=1024):
+    spectrogram = librosa.core.stft(y=y, n_fft=frame_length, hop_length=hop_length)
+    power_spectrogram = (np.abs(spectrogram))**2
+    """"
+    print(spectrogram[0])
+    print()
+    """
+    # print(len(power_spectrogram), len(power_spectrogram[0]))
+    # print('spectrogram first element:\n\n', spectrogram_real[0], '\n\n\n')
+    return power_spectrogram
+
+
+# apply mel scale to spectrogram (with 40 bins, also try 80)
+def mel_transform(spectrogram, bin_number=40):
+    mel_spectrogram = librosa.feature.melspectrogram(S=spectrogram, n_mels=bin_number)
+    # print(len(mel_spectrogram), len(mel_spectrogram[0]))
+    # print('mel spectrogram first element:\n\n', mel_spectrogram[0], '\n\n\n')
+    return mel_spectrogram
+
+
+# apply a log10 scale to spectrogram (resulting magnitudes are in decibels)
+def log_scale(spectrogram):
+    log_mel_spectrogram = librosa.power_to_db(S=spectrogram)
+    # print(len(log_mel_spectrogram), len(log_mel_spectrogram[0]))
+    # print('log mel spectrogram first element:\n\n', log_mel_spectrogram[0], '\n\n\n')
+    return log_mel_spectrogram
+
+
+def compute_spectrogram(y, frame_length=2048, hop_length=1024, bin_number=40,
+                        printDimensions=False, printFirstFrame=False, printFirstBin=False):
+
+    ps = compute_power_spectrogram(y, frame_length=frame_length, hop_length=hop_length)
+    mps = mel_transform(ps, bin_number=bin_number)
+    lmps = log_scale(mps)
+
+    if printDimensions:
+        print('\ndimensions:\n\n', len(lmps), len(lmps[0]), '\n')
+    if printFirstFrame:
+        print('log mel spectrogram first frame:\n')
+        for i, f in enumerate(lmps):
+            print(i, f[0])
+    if printFirstBin:
+        print('\nlog mel spectrogram first freq. bin:\n\n', lmps[0], '\n\n\n')
+
+    return lmps
