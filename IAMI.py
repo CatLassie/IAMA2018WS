1bfe1212a25277125ca18d82ee8c058a6f8bb08d
Peter Ryabchenko <ryabchenko.peter@gmail.com>
2019-01-13 21:00:07 +0100

    removed spectrogram normalization

--- IAMI.py
+++ IAMI.py
@@ -7,10 +7,17 @@ import onset as o
 def main():
     file_path = 'music_data/shortName.flac'
     y = f.load_audio(file_path)
+
+    # COMPUTE SPECTROGRAM
     log_mel_spectrogram = f.compute_spectrogram(y, 2048, 1024, 40)
-    norm_spectrogram = o.normalize_frequencies(log_mel_spectrogram)
-    odf = o.compute_odf(norm_spectrogram)
-    peaks = o.apply_threshold(odf, 0.8)
+
+    # COMPUTE ONSET DETECTION FUNCTION
+    # skip normalization
+    # norm_spectrogram = o.normalize_frequencies(log_mel_spectrogram)
+    odf = o.compute_odf(log_mel_spectrogram) #o.compute_odf(norm_spectrogram)
+
+    # DETECT ONSETS
+    peaks = o.apply_threshold(odf, 1500)
     print(odf)
     # print(peaks)
     for i, p in enumerate(peaks):
