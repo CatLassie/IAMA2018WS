cb5ac7b2d83ff8467a62dc93db92bf95991f4af9
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2019-02-12 02:06:22 +0100

    added audio loading and ground truth loading (NOT FINISHED)

--- ex1/notebooks/customScripts/utilities.py
+++ ex1/notebooks/customScripts/utilities.py
@@ -15,13 +15,20 @@ def get_file_paths(folder_path):
 
     beats_audio_paths = []
     for i, file in enumerate(beats_gt_paths):
-        beats_audio_paths.append(file.split('onsets')[0]+'flac')
+        beats_audio_paths.append(file.split('beats')[0]+'flac')
 
     bpm_audio_paths = []
     for i, file in enumerate(bpm_gt_paths):
-        bpm_audio_paths.append(file.split('onsets')[0]+'flac')
+        bpm_audio_paths.append(file.split('bpm')[0]+'flac')
 
     return onsets_gt_paths, beats_gt_paths, bpm_gt_paths, onsets_audio_paths, beats_audio_paths, bpm_audio_paths
+
+def load_audios(file_paths, sampling_rate, inf=False):
+    audios = []
+    for i, path in enumerate(file_paths):
+        audios.append(load_audio(path, sampling_rate, inf))
+    return audios
+
 
 def load_audio(file_path, sampling_rate, inf=False):
     y, sr = librosa.load(file_path, sr=sampling_rate)
@@ -31,6 +38,13 @@ def load_audio(file_path, sampling_rate, inf=False):
         print("sampling rate: ", sr, '\n\n\n')
     return y
 
+def load_ground_truths(file_paths, delimiter):
+    gts = []
+    for i, path in enumerate(file_paths):
+        gts.append(load_ground_truth(path, delimiter))
+    return gts
+
+# TODO INT IS NOT GOOD FOR ONSETS AND BEATS, BPM IS NOT AN ARRAY!
 def load_ground_truth(file_path, delimiter):
     gt = np.genfromtxt(fname=file_path, delimiter=delimiter)
     return gt.astype(np.int64)
